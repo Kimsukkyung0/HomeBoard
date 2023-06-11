5be4a946b77ecbd8b59c474ac714550d69221a27
Min kyung suk <smk93021@gmail.com>
2023-06-11 23:51:13 +0900

    Process to add some Todo function

--- src/main/java/com/example/todo/TodoMapper.java
+++ src/main/java/com/example/todo/TodoMapper.java
@@ -1,4 +1,7 @@
 package com.example.todo;
 
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
 public interface TodoMapper {
 }
--- src/main/java/com/example/todo/TodoService.java
+++ src/main/java/com/example/todo/TodoService.java
@@ -1,4 +1,7 @@
 package com.example.todo;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class TodoService {
 }
